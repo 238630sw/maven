2752d83f6f27ad2f61d89c7d3f298102b5ea0b0e
sw <2386301889@qq.com>
2022-07-30 15:04:47 +0800

    first commit

---
+++ mybatis_helloworld/src/main/java/com/atguigu/mybatis/mapper/GitTest.java
@@ -0,0 +1,14 @@
+package com.atguigu.mybatis.mapper;
+
+/**
+ * @auther sw
+ * @create 2022-07-30 14:59
+ * @describe
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("fadfad");
+        System.out.println("fadffaad");
+
+    }
+}
