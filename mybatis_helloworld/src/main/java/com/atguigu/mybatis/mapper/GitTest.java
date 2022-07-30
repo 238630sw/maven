1e96a8d1fe98bbc8d2f1b6329ad9bdf1419b1729
sw <2386301889@qq.com>
2022-07-30 15:20:30 +0800

    second commit

--- mybatis_helloworld/src/main/java/com/atguigu/mybatis/mapper/GitTest.java
+++ mybatis_helloworld/src/main/java/com/atguigu/mybatis/mapper/GitTest.java
@@ -9,6 +9,7 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("fadfad");
         System.out.println("fadffaad");
+        System.out.println("abcde");
 
     }
 }
